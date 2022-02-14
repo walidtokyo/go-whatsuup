8c2d83e2570ecdd6bed32d5dbfc62c70e60d2f05
walidtokyo <69719539+walidtokyo@users.noreply.github.com>
2022-02-14 13:47:37 +0100

    Update handler.go

--- handler.go
+++ handler.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"strings"
 
-	"github.com/Rhymen/go-whatsapp/binary"
-	"github.com/Rhymen/go-whatsapp/binary/proto"
+	"github.com/walidtokyo/go-whatsuup/binary"
+	"github.com/walidtokyo/go-whatsuup/binary/proto"
 )
 
 /*
