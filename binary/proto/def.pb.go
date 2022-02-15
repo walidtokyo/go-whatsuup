04bdaa639e71af0f100c8d373aa92fe08187d9bb
walidtokyo <69719539+walidtokyo@users.noreply.github.com>
2022-02-15 09:19:16 +0100

    Update def.pb.go

--- binary/proto/def.pb.go
+++ binary/proto/def.pb.go
@@ -7,7 +7,6 @@
 package proto
 
 import (
-	proto "google.golang.org/protobuf/proto"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
